a548b9dfae1bd9ea21f0a96de8e812df988f643e
Brian Nguyen <bp.nguyen05@gmail.com>
2021-11-09 15:06:18 -0600

    Added new assets and made edits to App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import {
     Text,
     Image,
     ScrollView,
-    TextInput
+    TextInput,
+    StyleSheet,
+    ImageBackground
 } from 'react-native';
 
 const Somm = () => {
@@ -14,24 +16,52 @@ const Somm = () => {
                     flex: 1,
                     justifyContent: 'center',
                     alignItems: 'center'
-                }}>
+            }}>
                 <Text>Welcome to</Text>
                 <Text>Somm</Text>
-                <Image 
-                    source = {{ uri: './Assets/BlankBottle.png' }}
-                    style = {{ width: 200, height: 200}}
-                />
+
+                <View style = {{
+                    flex: 1, 
+                    height: 500
+                }}>
+                    <Image 
+                        source = {require('./Assets/BlankBottle.png')}
+                        style = {{ 
+                            flex: 1,
+                            resizeMode: 'contain'
+                        }}
+                    />
+                </View>
             </View>
             <TextInput
                 style = {{
                     height: 40, 
                     borderColor: 'gray',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    textAlign: 'center'
                 }}
                 defaultValue = 'Please type something in here!'
             />
         </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    text: {
+        color: 'white',
+        fontSize: 36,
+        lineHeight: 84,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        backgroundColor: '#000000c0'
+    }
+})
 
 export default Somm;
