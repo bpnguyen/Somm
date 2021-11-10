0ab8da66e5a890dfd5ca8dd9b16cf3e7249393d0
Brian Nguyen <bp.nguyen05@gmail.com>
2021-11-10 16:36:34 -0600

    Created route to home.js

--- App.js
+++ App.js
@@ -1,67 +1,10 @@
 import React from 'react';
-import { 
-    View, 
-    Text,
-    Image,
-    ScrollView,
-    TextInput,
-    StyleSheet,
-    ImageBackground
-} from 'react-native';
+import Home from './Home/home';
 
 const Somm = () => {
     return (
-        <ScrollView>
-            <View style = {{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-            }}>
-                <Text>Welcome to</Text>
-                <Text>Somm</Text>
-
-                <View style = {{
-                    flex: 1, 
-                    height: 500
-                }}>
-                    <Image 
-                        source = {require('./Assets/BlankBottle.png')}
-                        style = {{ 
-                            flex: 1,
-                            resizeMode: 'contain'
-                        }}
-                    />
-                </View>
-            </View>
-            <TextInput
-                style = {{
-                    height: 40, 
-                    borderColor: 'gray',
-                    borderWidth: 1,
-                    textAlign: 'center'
-                }}
-                defaultValue = 'Please type something in here!'
-            />
-        </ScrollView>
+        <Home />
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    image: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    text: {
-        color: 'white',
-        fontSize: 36,
-        lineHeight: 84,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        backgroundColor: '#000000c0'
-    }
-})
 
 export default Somm;
