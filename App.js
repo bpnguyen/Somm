530a5367e705a4710c8d7f721ca4e39d077f3a39
Brian Nguyen <bp.nguyen05@gmail.com>
2021-11-18 16:32:44 -0600

    Added navigation functionalities

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './src/Screens/Home/home.js';
 import Reveal from './src/Screens/Reveal/reveal.js';
 
+const Stack = createStackNavigator();
+
 const Somm = () => {
+    const [atHome, setAtHome] = React.useState(true);
+    const [atFavorites, setAtFavorites] = React.useState(false);
+
     return (
-        <Home />
-        //<Reveal />
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}>
+                <Stack.Screen
+                    name='Home'
+                    component={Home}
+                />
+                <Stack.Screen
+                    name='Reveal'
+                    component={Reveal}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
