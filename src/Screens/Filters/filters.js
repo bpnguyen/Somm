2e60f2729007e257435349280c0a10b7c0c5d12a
Adeline <47280713+Dadelinee@users.noreply.github.com>
2021-12-06 00:41:58 -0600

    managed to get a random wine to show for reveal page at least :'^)

--- src/Screens/Filters/filters.js
+++ src/Screens/Filters/filters.js
@@ -1,7 +1,7 @@
 /* 
 * Somm - Filters Screen 
 * 
-* Creator: 
+* Creator: Adeline Azungue (aazungue@luc.edu)
 */
 
 import React from 'react';
@@ -9,11 +9,17 @@ import {
     View,
     Text,
     Image,
-    Pressable
+    Pressable,
+    TextInput,
 } from 'react-native';
 
 import styles from './styles.js';
 import HeaderBar from '../../Components/HeaderBar/headerbar.js';
+import DropDownPicker from 'react-native-dropdown-picker';
+import winesJson from '../../Assets/Data/Wines.json';
+import text from '../../Components/text.js';
+
+
 
 const Filters = (props) => {
     const { navigation } = props;
@@ -22,9 +28,100 @@ const Filters = (props) => {
         navigation.navigate('Reveal');
     };
 
+
+    var wineArr = [];
+    var regionArr = [];
+    var countryArr = [];
+
+    //componenetWillMount()
+    const filterType = () => {
+        for (var i = 0; i < winesJson.length; i++) {
+            var type = winesJson[i].Type
+            //if(wineArr.contains(type)){
+            wineArr.push(type)
+        } //else{}
+
+        console.log(wineArr)
+        return wineArr;
+    }
+
+    var wanted = wineArr.filter(function (item) { return (wineArr.Type == 'Red'); });
+
+    const filterRegion = () => {
+        for (var i = 0; i < winesJson.length; i++) {
+            var region = winesJson[i].Region
+            regionArr.push(region)
+        }
+    }
+
+    const filterCountry = () => {
+        for (var i = 0; i < winesJson.length; i++) {
+            var country = winesJson[i].Country
+            countryArr.push(country)
+        }
+    }
+
+
+    const parseJSON = () => {
+        console.log(winesJson[0]); // Print first element
+        console.log(winesJson[0].Type); // Print 'Type' value of first element
+    };
+
+//     const result = winesJson.filter(type => type.Type == "Red");
+//     const random = result[Math.floor(Math.random()*result.length)];
+//     console.log(random);
+
+// let input = '';
+// let filterArr = [];
+
+// const UserInput = () => {
+//     //let input = '';
+//     const saveUserInput = userInput => {
+//       input = userInput;
+    
+//       //setTimeout(() => {  console.log(input); }, 2000); 
+       
+//     };
+
+//         return (
+//             <View>
+//                 <TextInput
+//                     style={styles.input}
+//                     onChangeText={userInput => saveUserInput(userInput)}
+//                 />
+//                 <TextInput
+//                     style={styles.input}
+//                 />
+//             </View>
+//         );
+//     };
+
+
     return (
         <View style={styles.container}>
-            <HeaderBar />
+            <HeaderBar/>
+        
+            <DropDownPicker
+                items={[
+                    {label: 'Red', value: 'red', selected: true},
+                    {label: 'White', value: 'white'},
+                    {label: 'Sparkling', value: 'sparkling'}
+
+                ]}
+                defaultNull
+                placeholder="Select Wine Color"
+                defaultIndex={0}
+                containerStyle={{ height: 40 }}
+                onChangeItem={item => console.log(item.label, item.value)}
+            />
+            {/* <UserInput
+                multiline
+                numberOfLines={1}
+                onChangeText={text => onChangeCountry(text)}
+                //value={value}
+                style={{ padding: 10 }}
+            /> */}
+
             <View style={styles.pressableContainer}>
                 <Pressable
                     style={styles.navigationPressable}
@@ -34,8 +131,20 @@ const Filters = (props) => {
                     </Text>
                 </Pressable>
             </View>
+
+            <View style={styles.pressableContainer}>
+                <Pressable
+                    style={styles.navigationPressable}
+                    onPress={() => App()}>
+                    <Text style={styles.navigationPressableText}>
+                        Test parseJSON function
+                    </Text>
+                </Pressable>
+            </View>
+
         </View>
     );
+
 }
 
 export default Filters;
